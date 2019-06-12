630bd8fafe4a859ceff4bdc8bde9df9394ceb5dd
Sang Hyeon <rlatkdgus500@gmail.com>
2019-06-13 01:10:07 +0900

    #Update demo

--- Assets/Demo/ColorStateModel.cs
+++ Assets/Demo/ColorStateModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorStateModel : RxState
+public class ColorStateModel : StateModel
 {
     [Header("Model Info")]
     public Color32 color = new Color32();
---
+++ Assets/Demo/TestPanelPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class TestPanelPresenter : MonoBehaviour {
+
+    private static string FORMAT_STRING = "State Machine Info\nColor : {0}";
+    public Text textPanel;
+
+    public Button redButton;
+    public Button greenButton;
+    public Button blueButton;
+
+    public ColorStateMachine machine;
+
+
+    public void Start() {
+
+        redButton.OnClickAsObservable()
+            .Subscribe(e => {
+                machine.TriggerStateTransition("To Red");
+            });
+
+        greenButton.OnClickAsObservable()
+            .Subscribe(e => {
+                machine.TriggerStateTransition("To Green");
+            });
+
+        blueButton.OnClickAsObservable()
+            .Subscribe(e => {
+                machine.TriggerStateTransition("To Blue");
+            });
+
+        machine.stateMachineAsObservable
+            .Subscribe(e => {
+                textPanel.text = string.Format(FORMAT_STRING, e.color.ToString());
+            });
+    }
+}
