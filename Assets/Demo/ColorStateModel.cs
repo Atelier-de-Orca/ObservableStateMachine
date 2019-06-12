cdcd94734aaa6b33cb84ebf2cc98042a0c407aec
Sang Hyeon <rlatkdgus500@gmail.com>
2019-06-12 23:31:28 +0900

    #Add demo code

---
+++ Assets/Demo/ColorStateModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorStateModel : RxState
+{
+    [Header("Model Info")]
+    public Color32 color = new Color32();
+}
