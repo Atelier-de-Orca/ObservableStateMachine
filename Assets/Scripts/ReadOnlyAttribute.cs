97955ee69777aedde7cbb255123265df90bb56fc
Sang Hyeon <rlatkdgus500@gmail.com>
2019-06-12 23:31:19 +0900

    #Add base code for state machine

---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[AttributeUsage(AttributeTargets.Field, Inherited = true)]
+public class ReadOnlyAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+[UnityEditor.CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyAttributeDrawer : UnityEditor.PropertyDrawer
+{
+    public override void OnGUI(Rect rect, UnityEditor.SerializedProperty prop, GUIContent label)
+    {
+        bool wasEnabled = GUI.enabled;
+        GUI.enabled = false;
+        UnityEditor.EditorGUI.PropertyField(rect, prop);
+        GUI.enabled = wasEnabled;
+    }
+}
+#endif
---
+++ Assets/Scripts/RxState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StringRxStatePair {
+    public string key;
+    public RxState value;
+}
+
+[Serializable]
+public class StateTransitionTrigger {
+    public string key;
+    public string value;
+}
+
+public class RxState : MonoBehaviour {
+
+    public List<StateTransitionTrigger> transitionTable;
+
+    public string GetTransitionState(string key) {
+        var ret = string.Empty;
+
+        foreach(var item in transitionTable) {
+            if (key.Equals(item.key)) {
+                ret = item.value;
+                break;
+            }
+        }
+
+        return ret;
+    }
+}
